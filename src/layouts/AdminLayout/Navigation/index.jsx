c44ae55db4c73b1df037afa68a33eccd1df47ec7
Devendra Mali <devendra.mali@digihost.in>
2024-09-19 10:30:53 +0530

    error fixed

--- src/layouts/AdminLayout/Navigation/index.jsx
+++ src/layouts/AdminLayout/Navigation/index.jsx
@@ -5,7 +5,7 @@ import useWindowSize from '../../../hooks/useWindowSize';
 
 import NavLogo from './NavLogo';
 import NavContent from './NavContent';
-import navigation from '../../../menu-items-bkp';
+import navigation from '../../../menu-items';
 
 const Navigation = () => {
   const configContext = useContext(ConfigContext);
