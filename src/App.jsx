060bb55320c4fa8ea24b4f7246ebb3c1a2dab251
BhavnaDigihost <bhavana.langi@digihost.in>
2024-09-24 12:25:27 +0530

    Menu and variable changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,11 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 
 import routes, { renderRoutes } from './routes';
+import Waves from 'node-waves';
+import 'node-waves/dist/waves.css';
 
 const App = () => {
   return <BrowserRouter basename={import.meta.env.VITE_APP_BASE_NAME}>{renderRoutes(routes)}</BrowserRouter>;
 };
-
+Waves.init();
 export default App;
