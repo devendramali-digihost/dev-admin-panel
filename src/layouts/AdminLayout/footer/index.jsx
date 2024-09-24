6963ae3678abcb477a984d8bed6241bec51c70a9
BhavnaDigihost <bhavana.langi@digihost.in>
2024-09-24 19:03:34 +0530

    footer create

---
+++ src/layouts/AdminLayout/footer/index.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Row, Col } from 'react-bootstrap';
+function Footer() {
+	return (
+		<React.Fragment>
+			<Row>
+				<footer>
+				<Col lg={6}>
+
+				</Col>
+				<Col lg={6}>
+					Crafted with <i class="mdi mdi-heart text-danger"></i> by <a href="https://1.envato.market/themesdesign" target="_blank">Themesdesign</a>
+				</Col>
+				</footer>
+			</Row>
+		</React.Fragment>
+	)
+}
+
+export default Footer
