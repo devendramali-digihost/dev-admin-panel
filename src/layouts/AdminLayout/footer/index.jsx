c6dee8eaafda5d0ddf2960618e94d752d14fc9b7
BhavnaDigihost <bhavana.langi@digihost.in>
2024-09-25 10:41:26 +0530

    update style

--- src/layouts/AdminLayout/footer/index.jsx
+++ src/layouts/AdminLayout/footer/index.jsx
@@ -3,16 +3,16 @@ import { Row, Col } from 'react-bootstrap';
 function Footer() {
 	return (
 		<React.Fragment>
-			<Row>
-				<footer>
-				<Col lg={6}>
-
-				</Col>
-				<Col lg={6}>
-					Crafted with <i class="mdi mdi-heart text-danger"></i> by <a href="https://1.envato.market/themesdesign" target="_blank">Themesdesign</a>
-				</Col>
-				</footer>
-			</Row>
+			<footer className="footer">
+				<Row>
+					<Col lg={6}>
+					&copy; {new Date().getFullYear()} Copyright:{' '} © DigiHost.
+					</Col>
+					<Col lg={6}>
+						Crafted with <i class="mdi mdi-heart text-danger"></i> by <a href="https://1.envato.market/themesdesign" target="_blank">DigiHost</a>
+					</Col>
+				</Row>
+			</footer>
 		</React.Fragment>
 	)
 }
