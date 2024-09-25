8d67dd9ef8816f5c6e2cbc8b8c9586e6f4a996fb
manisha-digihost <manisha.prajapati@digihost.in>
2024-09-25 16:17:17 +0530

    added domain verify style

--- src/views/company/DomainVerify.jsx
+++ src/views/company/DomainVerify.jsx
@@ -16,27 +16,52 @@ const DomainVerify = () => {
                   </Form.Group>
                 </Col>
                 <Col lg={6}>
-                  <Form.Group className="mb-3" controlId="CompanyIndustry">
-                    <Form.Label>Company Industry</Form.Label>
-                    <Form.Select aria-label="Company Industry">
-                      <option>Select Industry</option>
-                      <option value="1">One</option>
-                      <option value="2">Two</option>
-                      <option value="3">Three</option>
-                    </Form.Select>
+                  <Form.Group className="mb-3" controlId="CompanyName">
+                    <Form.Label>Domain ID</Form.Label>
+                    <Form.Control type="text" placeholder="Domain ID" />
                   </Form.Group>
                 </Col>
                 <Col lg={6}>
-                  <Form.Group className="mb-3" controlId="CompanyDescription">
-                    <Form.Label>Company Description</Form.Label>
-                    <Form.Control as="textarea" rows={1} placeholder="Add Company Description" />
+                  <Form.Group className="mb-3" controlId="CompanyName">
+                    <Form.Label>Domain Password</Form.Label>
+                    <Form.Control type="password" placeholder="Company Name" />
                   </Form.Group>
                 </Col>
-                <Col lg={6}>
-                  <Form.Group className="mb-3" controlId="CompanyLogo">
-                    <Form.Label>Upload Logo</Form.Label>
-                    <Form.Control type="file" placeholder="Company Name" />
-                  </Form.Group>
+                <Col lg={12}>
+                  <div className="text-start">
+                    <button type="submit" className="btn btn-primary" variant="primary">
+                      Verify
+                    </button>
+                  </div>
+                  <div className="details">
+                    <h4>Details</h4>
+                    <ul>
+                      <li className="info">
+                        <span className="dt">Is the domain correct?: </span>
+                        <span>Yes</span>
+                      </li>
+                      <li className="info">
+                        <span className="dt">Registry Domain ID: </span>
+                        <span>HJIW4928</span>
+                      </li>
+                      <li className="info">
+                        <span className="dt">Registrar URL: </span>
+                        <span>HJIW4928</span>
+                      </li>
+                      <li className="info">
+                        <span className="dt">Updated Date: </span>
+                        <span>25th September 2024</span>
+                      </li>
+                      <li className="info">
+                        <span className="dt">Creation Date: </span>
+                        <span>25th September 2024</span>
+                      </li>
+                      <li className="info">
+                        <span className="dt">Registrar Registration Expiration Date: </span>
+                        <span>25th September 2024</span>
+                      </li>
+                    </ul>
+                  </div>
                 </Col>
               </Row>
             </Form>
