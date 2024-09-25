4eee30755873b7a158d9f9aeac4b46da955d9dd2
Devendra Mali <devendra.mali@digihost.in>
2024-09-25 18:06:16 +0530

    form temp

---
+++ src/views/admin-tamplate/FormTamplate.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const FormTamplate = () => {
+  return (
+    <>
+        <Row>
+            <Col lg={12}>
+                <Card>
+                    <Card.Body>
+                        <Form>
+                            <Row>
+                                
+                            </Row>
+                        </Form>
+                    </Card.Body>
+                </Card>
+            </Col>
+        </Row>
+    </>
+  )
+}
+
+export default FormTamplate
