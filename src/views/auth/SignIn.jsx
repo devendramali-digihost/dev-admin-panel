b2ae99d696281d0ff62836597e6f78305fd96e0c
sneha-digihost <sneha.sarang@digihost.in>
2024-10-18 13:41:07 +0530

    dev panel ui of multistep form

--- src/views/auth/SignIn.jsx
+++ src/views/auth/SignIn.jsx
@@ -1,48 +1,50 @@
 import React from 'react';
-import { Card } from 'react-bootstrap';
-import { Row, Col, Container } from 'react-bootstrap';
+import Card from 'react-bootstrap/Card';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 import { NavLink } from 'react-router-dom';
-import loginImage from '../../assets/images/auth/login.jpg';
+import loginImage from '../../assets/images/auth/login.jpg';  // Convert to WebP for better performance
 import logo from '../../assets/images/auth/logo.png';
 import AuthLogin from './JWTLogin';
-
 
 const SignIn = () => {
   return (
     <React.Fragment>
-        <div className="auth-wrapper">
-            <Row className="auth-row">
-                <Col sm={6} className="image p-0">
-                    <img src={loginImage} alt="" />
-                    <div className='copyright_form'>
-                        <p>Copyright © {new Date().getFullYear()} Dev Panel. All Rights Reserved. Powered By: <NavLink to={'https://www.digihost.in/'} targe="_blank">DigiHost Tech Solutions Pvt. Ltd.</NavLink></p>
+      <div className="auth-wrapper">
+        <Row className="auth-row">
+          <Col sm={6} className="image p-0">
+            <img
+              src={loginImage}
+              alt="Login"
+              loading="lazy"  // Lazy load non-essential images
+              width="600"
+              height="400"
+            />
+            <div className='copyright_form'>
+              <p>
+                Copyright © {new Date().getFullYear()} Dev Panel. All Rights Reserved.
+                Powered By: <NavLink to={'https://www.digihost.in/'} target="_blank">DigiHost Tech Solutions Pvt. Ltd.</NavLink>
+              </p>
+            </div>
+          </Col>
+          <Col sm={6} className="p-0">
+            <div className="auth-content">
+              <Card className="borderless d-flex align-items-center justify-content-center">
+                <Card.Body>
+                  <div className="mb-4 text-center">
+                    <div className="logo">
+                      <img src={logo} alt="Logo" width="200" height="50" />  {/* Preload the logo */}
                     </div>
-                </Col>
-                <Col sm={6} className="p-0">
-                    <div className="auth-content">
-                        <Card className="borderless d-flex align-items-center justify-content-center">
-                            <Card.Body className="">
-                                <div className="mb-4 text-center">
-                                    <div className="logo">
-                                        <img src={logo} alt="" />
-                                    </div>
-                                    <h5 className="mt-1">Welcome Back !</h5>
-                                    <p>Sign in to continue to DiigiiHost.</p>
-                                </div>
-                                <AuthLogin />
-
-                                {/* <div className="below-text">
-                                    <p>
-                                        Don't have an account ? <NavLink to={'#'}>Register</NavLink>{' '}
-                                    </p>
-
-                                </div> */}
-                            </Card.Body>
-                        </Card>
-                    </div>
-                </Col>
-            </Row>
-        </div>
+                    <h5 className="mt-1">Welcome Back!</h5>
+                    <p>Sign in to continue to DiigiiHost.</p>
+                  </div>
+                  <AuthLogin />
+                </Card.Body>
+              </Card>
+            </div>
+          </Col>
+        </Row>
+      </div>
     </React.Fragment>
   );
 };
