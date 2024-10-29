b2f92c2d91686c154ca177202587cbe3694a5c9d
sneha-digihost <sneha.sarang@digihost.in>
2024-10-29 12:57:10 +0530

    UI new built

--- src/views/dev-forms/AdminPanelTemplate.jsx
+++ src/views/dev-forms/AdminPanelTemplate.jsx
@@ -1,10 +1,8 @@
 import React, { useState } from 'react';
 import { Row, Col, Form, Card } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
-
 import { FaSearch, FaEdit, FaThLarge, FaRegEye } from 'react-icons/fa';
 import { admin1, admin2 } from '../../images';
-
 
 export const AdminPanelTemplate = () => {
   const [selectedOption, setSelectedOption] = useState('');
@@ -32,18 +30,18 @@ export const AdminPanelTemplate = () => {
             <Form>
               <Row>
                 <Col lg={4}>
-                <Form.Group className="mb-3" controlId="DomainName">
-                  <div className="input-group rounded search-icon">
-                    <Form.Control
-                      type="text"
-                      placeholder="Search template by"
-                      className="" // Make the end of the input rounded
-                    />
-                    <span className="input-group-text"> {/* Make the start of the icon rounded */}
-                      <FaSearch />
-                    </span>
-                  </div>
-                 </Form.Group>
+                  <Form.Group className="mb-3" controlId="DomainName">
+                    <div className="input-group rounded">
+                      <Form.Control
+                        type="text"
+                        placeholder="Search template by"
+                        className="rounded-start" // Rounded start
+                      />
+                      <span className="input-group-text rounded-end"> {/* Rounded end for the icon */}
+                        <FaSearch />
+                      </span>
+                    </div>
+                  </Form.Group>
                 </Col>
                 <Col lg={8}>
                   <div className="form-btn-panel">
@@ -107,7 +105,7 @@ export const AdminPanelTemplate = () => {
                     <div className="card-image-wrapper">
                       <Card.Img variant="top" src={admin2} />
                       <div className="overlay">
-                      <div className="button">
+                        <div className="button">
                           <a href="#" target="_blank" className="create-button btn btn-primary waves-effect waves-light">
                             Preview
                           </a>
@@ -137,7 +135,6 @@ export const AdminPanelTemplate = () => {
           </Col>
         </Row>
       </div>
-
     </React.Fragment>
   );
 };
