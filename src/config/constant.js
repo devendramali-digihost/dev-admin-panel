5cf8d05ec786940c6009b8a94b8875384009aa48
manisha-digihost <manisha.prajapati@digihost.in>
2024-11-08 15:32:41 +0530

    check vercel link issue

--- src/config/constant.js
+++ src/config/constant.js
@@ -1,7 +1,7 @@
 // export const BASE_URL = '/app/dashboard/default';
-export const BASE_URL = '/auth/signin';
-export const BASE_TITLE = ' DigiiHost Dev Panel';
+export const BASE_URL = "/auth/signin";
+export const BASE_TITLE = "DiigiiHost Dev Panel";
 
 export const CONFIG = {
-  layout: 'vertical'
+  layout: "vertical",
 };
